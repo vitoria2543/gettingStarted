6d6ba8401f43008d9430c68508f85239dba4c1c1
Vitoria De Souza <vitoriadesouza@Vitorias-MBP.fios-router.home>
2026-09-09 13:25:15 -0400

    Complete getting started assignment

---
+++ gettingStarted.py
@@ -0,0 +1,38 @@
+def welcome_assignment_answers(question):
+
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "Yes"
+
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "8eab0710c8eaef078e761eb8f37cd68eb237c672b3b2385b595b80f786790691"
+
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 2
+
+    else:
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+
+    return answer
+
+
+if __name__ == "__main__":
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
