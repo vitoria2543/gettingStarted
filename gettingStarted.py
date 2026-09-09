b13948a7e5fc0199ee7998f9da5fa73d40879d8e
Vitoria De Souza <vitoriadesouza@Vitorias-MBP.fios-router.home>
2026-09-09 13:42:32 -0400

    Fix assignment answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
         answer = "Yes"
 
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
 
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
@@ -22,10 +22,10 @@ def welcome_assignment_answers(question):
         answer = "No"
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 4
+        answer = 5
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 3
 
     else:
         answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
